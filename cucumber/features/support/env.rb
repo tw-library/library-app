9ad396038d4eae817d2bc76d3ca675d9706989e2
Lucas Avila <lavila@thoughtworks.com>
2015-06-26 14:12:05 -0300

    Adicionado suporte para rodar testes automatizados em chrome e firefox

--- cucumber/features/support/env.rb
+++ cucumber/features/support/env.rb
@@ -1,7 +1,14 @@
 require 'selenium-webdriver'
 
 Before do
-  @browser = Selenium::WebDriver.for :chrome
+	case ENV['BROWSER']
+	when 'chrome'
+		@browser = Selenium::WebDriver.for :chrome
+	when 'firefox'
+		@browser = Selenium::WebDriver.for :firefox
+	else
+		@browser = Selenium::WebDriver.for :chrome		
+	end	
 end
 
 After do
